39bd500572e57aa998b660c07b0f3b021976bb12
Kyle Johnson <ky.j.johnson92@gmail.com>
2020-12-10 15:29:22 -0500

    minor bug fixes, code refactor

--- calculator.js
+++ calculator.js
@@ -1,77 +1,100 @@
 let digits = document.querySelectorAll('.digit')
-let screen = document.querySelector('.screen')
+let operations = document.querySelector('.previous')
+let screen = document.querySelector('.current')
 let operators = document.querySelectorAll('.operator')
 let clearBtn = document.querySelector('#all-clear')
 let computeBtn = document.querySelector('#compute')
+let operation;
 let currentVal ='';
 let previousVal;
 let operand;
 let clicked;
 
-computeBtn.addEventListener('click', function(){
+
+//Functionality when clicking to compute
+computeBtn.addEventListener('click', () => {
     let val = compute()
-    screen.innerText = val
+    updateScreen(val)
     currentVal = val
+    updateOperation()
+
 })
 
-operators.forEach(operator => operator.addEventListener('click', function(){
+//functionality when a operator button is clicked
+operators.forEach(operator => operator.addEventListener('click', ()=>{
+    resetColors()
     operator.style.backgroundColor = '#FA7A35'
+    operation = previousVal
     previousVal = currentVal
     currentVal = ''
-    operand = this.innerText
+    operand = operator.innerText
     clicked = true
-    console.log(currentVal)
-    console.log(previousVal)
-    console.log(operand)
 }));
 
-
-digits.forEach(digit => digit.addEventListener('click', function(){
-    numConcat(this.value)
-    updateScreen()
+//Functionality when a numeral is clicked
+digits.forEach(digit => digit.addEventListener('click', () => {
+    if(digit.value == '.' && !currentVal){
+        currentVal = '0'
+    }
+    numConcat(digit.value)
+    updateScreen(currentVal)
     resetColors()
-    console.log(currentVal)
-    console.log(previousVal)
 }));
 
-clearBtn.addEventListener('click',function(){
-    currentVal = '0'
-    screen.innerText = '0'
+//Functionality when clearing calculator
+clearBtn.addEventListener('click', () => {
+    currentVal = ''
+    updateScreen('0')
     resetColors()
+    operations.innerText = ''
+
 
 })
 
-function updateScreen(){
-    screen.innerText = currentVal
+//To update screen when current numeral is clicked
+function updateScreen(val){
+    screen.innerText = val
 }
 
+function updateOperation(){
+    if (!operation){
+        operations.innerText = previousVal + operand + currentVal
+    } else {
+        operations.innerText = operation
+    }
+}
+
+//To allow >single digit values ie: 4332
 function numConcat(val){
     currentVal = currentVal + val
 }
 
+function operationConcat(){
+    operation = operation + operand + previousVal
+}
+
+//To clear the color of a clicked operator
 function resetColors(){
     if(clicked === true){
         operators.forEach(operator => operator.removeAttribute("style"));
         clicked = false
     }   
 }
-
+//Computation logic
 function compute(){
     let prev = parseFloat(previousVal)
     let current = parseFloat(currentVal)
     switch(operand){
         case '+':
              return( prev + current)
-             break
         case '-':
             return prev - current
-            break
         case 'x':
             return prev * current
-            break
         case '÷':
             return prev / current
-            break   
+        default:
+            break
     }
 
 }
